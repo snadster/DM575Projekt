23ef8f12a56b304d2689108a191ad500f27f3a3f
Sandra K. <sjoha23@student.sdu.dk>
2024-04-22 16:13:07 +0200

    added circle and comments

--- demo/src/main/java/com/example/App.java
+++ demo/src/main/java/com/example/App.java
@@ -8,6 +8,7 @@ import javafx.scene.image.Image;
 import javafx.scene.input.KeyCombination;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
 import javafx.scene.text.Font;
@@ -31,13 +32,25 @@ public class App extends Application {
         Scene scene = new Scene(root,960,672,Color.DARKGRAY);
 
         // make text, set size font and color
+        // this text and the following circle are for show
+        // the finished program will need smaller text to show highscore, but not any circle or the like.
+        // we might need to use canvas instead to be able to render our stuff, so this is very just for show
         Text text = new Text();
         text.setText("pac-man :)");
         text.setX(350);
         text.setY(100);
         text.setFont(Font.font("Brush Script",60));
-        text.setFill(Color.YELLOW);
+        text.setFill(Color.YELLOWGREEN);
         root.getChildren().add(text);
+
+        Circle paccy = new Circle();
+        paccy.setCenterX(480);
+        paccy.setCenterY(336);
+        paccy.setRadius(50);
+        paccy.setFill(Color.YELLOW);
+        root.getChildren().add(paccy);
+
+
         
         stage.setTitle("pac man?");
         stage.setScene(scene);
